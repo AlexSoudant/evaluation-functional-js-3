48c47c57e142942a6681d5183a33af648917cf24
Alex Soudant <soudant.alex@gmail.com>
2018-03-02 10:24:39 +0100

    Exercice 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -1,8 +1,21 @@
 const countSequence = (sequence, input) => {
   // Your future job begins here ...
+  
+
+   return sequence ? input.split(sequence).length - 1 : 0
 };
+
+function curry(countSequence){ 
+  return function(sequence) {
+    return function(input) {
+      return countSequence(sequence, input)
+    }
+  }
+}
+
+const curried_count = curry(countSequence)
 
 module.exports = {
   title: "Exercise 4",
-  run: countSequence
+  run: curried_count
 };
